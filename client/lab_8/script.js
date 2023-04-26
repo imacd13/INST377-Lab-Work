df84d6b7e0321241c49fe249df114cd52406a6f6
Isabella Macdonald <imacd13@terpmail.umd.edu>
2023-04-25 22:31:30 -0400

    JS fixes, add JSON parsing

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -58,7 +58,11 @@ async function mainEvent() {
   loadAnimation.style.display = "none";
   generateListButton.classList.add("hidden");
 
-  let storedList = [];
+  const storedData = localStorage.getItem('storedData');
+  const parsedData = JSON.parse(storedData);
+  if (parsedData.length > 0) {
+    generateListButton.classList.remove("hidden");
+  };
 
   let currentList = []; // this is "scoped" to the main event function
 
@@ -80,11 +84,8 @@ async function mainEvent() {
     );
 
     // This changes the response from the GET into data we can use - an "object"
-    storedList = await results.json();
-
-    if (storedList.length > 0) {
-      generateListButton.classList.remove("hidden");
-    }
+    const storedList = await results.json();
+    localStorage.setItem('storedData', JSON.stringify(storedList));
 
     loadAnimation.style.display = "none";
     console.table(storedList);
@@ -103,7 +104,7 @@ async function mainEvent() {
 
   generateListButton.addEventListener("click", (event) => {
     console.log("generate new list");
-    currentList = cutRestaurantList(storedList);
+    currentList = cutRestaurantList(parsedData);
     console.log(currentList);
     injectHTML(currentList);
   });
